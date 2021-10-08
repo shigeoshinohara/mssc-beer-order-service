3aabf35fb882528ca3706d7f0210bfc22e7cd010
ShigeoShinohara <s.shinohara@sync-web.jp>
2021-10-08 18:32:44 +0900

    add OrderService

---
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerDto.java
@@ -0,0 +1,36 @@
+package guru.sfg.beer.order.service.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    @Null
+    private UUID id;
+
+    @NotBlank
+    private String beerName;
+
+    @NotBlank
+    private String beerStyle;
+
+    @Positive
+    private Long upc;
+
+    @Positive
+    private BigDecimal price;
+
+}
+
